fe930d0c0c02dd4eef8ae9d7ca375dd97ec5d297
Bruno <bruno.mestres@poscontrole.com.br>
2026-03-29 19:04:50 -0300

    feat: vitest coverage

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,13 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async create(data) {
-        return {
-          id: 'user-id',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-      async findByEmail(email) {
-        return null
-      },
-    })
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
 
     const user = await registerUseCase.execute({
       name: 'John Doe',
@@ -29,5 +19,38 @@ describe('Register Use Case', () => {
       user.user.password_hash,
     )
     expect(isPasswordHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const email = 'johndoe@example.com'
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: 'password123',
+    })
+
+    await expect(
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: 'password123',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should be able to register', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password123',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
   })
 })
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'node:path'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    coverage: {
+      provider: 'v8',
+    },
+  },
 })
